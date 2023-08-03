d19afde195c2e006cbf7f242ebf03541fea74abd
viveks <blackarck@gmail.com>
2023-08-03 12:40:18 +0530

    movement working ok

--- main.js
+++ main.js
@@ -6,73 +6,80 @@ document.addEventListener("DOMContentLoaded", function () {
 
   initKeys();
 
-  let snakeBod = 3; // initial length of snake body
+  let snakeBod = 6; // initial length of snake body
   let snakefull = []; // array to hold the snake body
   let dirx=0;
   let diry=0;
+  let grid=15;
   //generate the snake body
 
   for (let i = snakeBod - 1; i >= 0; i--) {
     let sprite = Sprite({
-      x: 20*i + 2*i, // starting x,y position of the sprite
-      y: 20,
-      color: generateRandomColor(), // fill color of the sprite rectangle
-      width: 20, // width and height of the sprite rectangle
-      height: 20,
-      //dx:  2, // move the sprite 2px to the right every frame
+      x: 15*i + 2*i, // starting x,y position of the sprite
+      y: 15,
+      color: generateRandomColor(), // random color to understand what is going on
+      width: grid, // width and height of the sprite rectangle
+      height: grid,
     });
-
     snakefull.push(sprite);
   }
   console.log("Snakefull length "+snakefull.length);
 
   let loop = GameLoop({
+    fps:15,
     // create the main game loop
     update: function () {
       // update the game state
       if (keyPressed("arrowleft")) {
         // move left
         //console.log("Moving left");
-        dirx=-2;
+        dirx=-grid;
         diry=0;
       }
       if (keyPressed("arrowright")) {
         // move left
        // console.log("Moving right");
-        dirx=2;
+        dirx=grid;
         diry=0;
       }
       if (keyPressed("arrowup")) {
         // move left
         //console.log("Moving up");
         dirx=0;
-        diry=-2;
+        diry=-grid;
       }
       if (keyPressed("arrowdown")) {
         // move left
         //console.log("Moving down");
         dirx=0;
-        diry=2;
+        diry=grid;
       }
       
-      //snake movement , move only head and transfer position
+      // snake movement , move only head and transfer position
       // to previous boxes for a pixel like movement
-      for (let i = 0; i < snakefull.length-1 ; i++) {
-        //transfer pos to previous box
-        snakefull[i+1].x = snakefull[i].x-(dirx*10);
-        snakefull[i+1].y = snakefull[i].y-(diry*10);
-     }
-
+     
       if( Math.abs(dirx) > 0 || Math.abs(diry) > 0){
-         snakefull[0].dx = dirx;
-         snakefull[0].dy = diry;
-      }
+        //move the tail to front depending on which direction we are 
+        //moving
+        snakefull[snakefull.length-1].x = snakefull[0].x + (dirx );
+        snakefull[snakefull.length-1].y = snakefull[0].y + (diry );
+  
+         //mark tail as head
+          headsprite=snakefull[0]; 
+          snakefull[0]=snakefull[snakefull.length-1];
 
-     // console.log("Dirx-"+dirx+", diry-"+diry);
-     // for (let i = snakeBod - 1; i >= 0; i--) {
-     //   snakefull[i].update();
-     // }
-      snakefull[0].update();
+        for (let i = snakefull.length-1; i > 1; i--) {
+        //transfer pos to previous box
+         snakefull[i] = snakefull[i-1]; 
+       }
+       snakefull[1]=headsprite;
+
+      }
+     
+      for (let i = snakeBod - 1; i >= 0; i--) {
+         snakefull[i].update();
+       }
+       
       // wrap the sprites position when it reaches
       // the edge of the screen
       for (let i = snakeBod - 1; i >= 0; i--) {
@@ -88,7 +95,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (snakefull[i].y <0) {
           snakefull[i].y = canvas.height;
         } //end of if
-        
       } //end of for
      
     }, //end of update function
@@ -102,16 +108,12 @@ document.addEventListener("DOMContentLoaded", function () {
   loop.start(); // start the game
 });
 
-
- 
 function generateRandomColor() {
   // Generate random values for RGB components (0 to 255)
   var red = Math.floor(Math.random() * 256);
   var green = Math.floor(Math.random() * 256);
   var blue = Math.floor(Math.random() * 256);
-
   // Combine RGB values into a CSS color string
   var color = "rgb(" + red + "," + green + "," + blue + ")";
-
   return color;
 }
